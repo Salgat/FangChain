bd98741c31e1f147fcd2107ab31d3eb22f5cb67d
Austin Salgat <salgat@salgat.net>
2022-02-18 12:25:46 -0600

    Refactor CLI to use the nested Commands
    Didn't realize you could just add commands to the RootCommand.

--- FangChain.CLI/Program.cs
+++ FangChain.CLI/Program.cs
@@ -12,88 +12,105 @@ services.AddTransient<IValidator, Validator>();
 services.AddTransient<ILoader, Loader>();
 var serviceProvider = services.BuildServiceProvider();
 
-var argument = new Argument<string>("command", "Command name.");
-var credentialsOption = new Option<string?>("--credentials-path", "The path of the credentials to create or read from. Defaults to current working directory.");
+var defaultDirectory = Directory.GetCurrentDirectory();
+var loader = serviceProvider.GetRequiredService<ILoader>();
+
+var credentialsOption = new Option<string?>("--credentials-path", "The path of the credentials to create. Defaults to current working directory.");
+var credentialsRequiredOption = new Option<string>("--credentials-path", "The path of the credentials to use.");
 var blockchainPathOption = new Option<string?>("--blockchain-path", "The path of the blockchain to create or read from. Defaults to the current working directory.");
-var transactionPathOption = new Option<string?>("--transaction-path", "The path of the transaction to read and write to.");
-var command = new RootCommand
-{
-    argument, 
-    credentialsOption,
-    blockchainPathOption,
-    transactionPathOption
-};
-command.Description = "FangChain CLI";
+var transactionPathOption = new Option<string>("--transaction-path", "The path of the transaction to read and write to.");
 
-command.SetHandler(async (string argument, string? credentialsPath, string? blockchainPath, string? transactionPath, CancellationToken cancellationToken) => 
-{
-    try
+#region Commands
+// Create Keys
+var createKeysCommand = 
+    new Command("create-keys", "Creates the public and private keys for a new user.")
     {
-        argument = argument.ToLowerInvariant();
-        var defaultDirectory = Directory.GetCurrentDirectory();
-        blockchainPath ??= defaultDirectory;
-        var loader = serviceProvider.GetRequiredService<ILoader>();
+        credentialsOption
+    };
+createKeysCommand.SetHandler(async (string? credentialsPath, CancellationToken cancellationToken) =>
+{
+    Console.WriteLine($"Creating public and private keys.");
+    var keyCreation = serviceProvider.GetRequiredService<IKeyCreation>();
+    var keys = keyCreation.CreatePublicAndPrivateKeys();
 
-        if (argument == "create-keys")
-        {
-            Console.WriteLine($"Creating public and private keys.");
-            var keyCreation = serviceProvider.GetRequiredService<IKeyCreation>();
-            var keys = keyCreation.CreatePublicAndPrivateKeys();
+    var destination = credentialsPath ?? Path.Join(defaultDirectory, $"keys-{DateTimeOffset.UtcNow:yyyyMMddTHHmmss}.json");
+    await keyCreation.StoreKeysAsync(destination, keys, cancellationToken);
+    Console.WriteLine($"Public and private keys stored in file path '{destination}'.");
+}, credentialsOption);
 
-            var destination = credentialsPath ?? Path.Join(defaultDirectory, $"keys-{DateTimeOffset.UtcNow:yyyyMMddTHHmmss}.json");
-            await keyCreation.StoreKeysAsync(destination, keys, cancellationToken);
-            Console.WriteLine($"Public and private keys stored in file path '{destination}'.");
-        }
-        else if (argument == "create-blockchain")
-        {
-            Console.WriteLine("Creating blockchain.");
-            if (credentialsPath is null)
-            {
-                throw new ArgumentException($"The '{credentialsOption.Name}' option must be populated with the base58 public key.");
-            }
-
-            var blockchainCreation = serviceProvider.GetRequiredService<IBlockchainCreation>();
-            var blockchainDirectory = new DirectoryInfo(blockchainPath);
-            await blockchainCreation.CreateBlockChainAsync(blockchainDirectory, credentialsPath, cancellationToken);
-            Console.WriteLine($"Blockchain created at location '{Path.GetFullPath(blockchainDirectory.FullName)}'.");
-        }
-        else if (argument == "validate-blockchain")
-        {
-            Console.WriteLine($"Validating blockchain at '{blockchainPath}'.");
-            var blockchain = await loader.LoadBlockchainAsync(blockchainPath, cancellationToken);
-
-            var validator = serviceProvider.GetRequiredService<IValidator>();
-            if (validator.IsBlockchainValid(blockchain))
-            {
-                Console.WriteLine($"Blockchain is valid.");
-            } 
-            else
-            {
-                Console.WriteLine($"Blockchain is invalid.");
-            }
-        }
-        else if (argument == "sign-transaction")
-        {
-            if (transactionPath is null)
-            {
-                throw new ArgumentException($"Transaction path must be provided.");
-            }
-
-            Console.WriteLine($"Signing transaction at '{transactionPath}'.");
-
-            var transaction = await loader.LoadTransactionAsync(transactionPath, cancellationToken);
-            var keys = await loader.LoadKeysAsync(credentialsPath, cancellationToken);
-            var signature = transaction.CreateSignature(PublicAndPrivateKeys.FromBase58(keys));
-            transaction.AddSignature(signature);
-            await File.WriteAllTextAsync(transactionPath, JObject.FromObject(transaction).ToString(), cancellationToken);
-
-            Console.WriteLine($"Transactioned signed by user '{signature.PublicKeyBase58}'.");
-        }
-    } 
-    catch (Exception e)
+// Create Blockchain
+var createBlockchainCommand = 
+    new Command("create-blockchain", "Creates a new blockchain where the credential provider is the initial authoritative user.")
     {
-        Console.WriteLine($"Error Occurred: {e.Message}");
+        credentialsOption,
+        blockchainPathOption
+    };
+createBlockchainCommand.SetHandler(async (string credentialsPath, string blockchainPath, CancellationToken cancellationToken) =>
+{
+    Console.WriteLine("Creating blockchain.");
+    if (credentialsPath is null)
+    {
+        throw new ArgumentException($"The '{credentialsOption.Name}' option must be populated with the base58 public key.");
     }
-}, argument, credentialsOption, blockchainPathOption, transactionPathOption);
 
-return await command.InvokeAsync(args);
+    var blockchainCreation = serviceProvider.GetRequiredService<IBlockchainCreation>();
+    var blockchainDirectory = new DirectoryInfo(blockchainPath);
+    await blockchainCreation.CreateBlockChainAsync(blockchainDirectory, credentialsPath, cancellationToken);
+    Console.WriteLine($"Blockchain created at location '{Path.GetFullPath(blockchainDirectory.FullName)}'.");
+}, credentialsRequiredOption, blockchainPathOption);
+
+// Validate Blockchain
+var validateBlockchainCommand =
+    new Command("validate-blockchain", "Validates the blockchain state, including the signatures for each block.")
+    {
+        blockchainPathOption
+    };
+validateBlockchainCommand.SetHandler(async (string blockchainPath, CancellationToken cancellationToken) =>
+{
+    blockchainPath ??= defaultDirectory;
+
+    Console.WriteLine($"Validating blockchain at '{blockchainPath}'.");
+    var blockchain = await loader.LoadBlockchainAsync(blockchainPath, cancellationToken);
+
+    var validator = serviceProvider.GetRequiredService<IValidator>();
+    if (validator.IsBlockchainValid(blockchain))
+    {
+        Console.WriteLine($"Blockchain is valid.");
+    }
+    else
+    {
+        Console.WriteLine($"Blockchain is invalid.");
+    }
+}, blockchainPathOption);
+
+// Sign Transaction
+var signTransactionCOmmand =
+    new Command("sign-transaction", "Adds a signature to the transaction.")
+    {
+        credentialsOption,
+        transactionPathOption
+    };
+signTransactionCOmmand.SetHandler(async (string credentialsPath, string transactionPath, CancellationToken cancellationToken) =>
+{
+    Console.WriteLine($"Signing transaction at '{transactionPath}'.");
+
+    var transaction = await loader.LoadTransactionAsync(transactionPath, cancellationToken);
+    var keys = await loader.LoadKeysAsync(credentialsPath, cancellationToken);
+    var signature = transaction.CreateSignature(PublicAndPrivateKeys.FromBase58(keys));
+    transaction.AddSignature(signature);
+    await File.WriteAllTextAsync(transactionPath, JObject.FromObject(transaction).ToString(), cancellationToken);
+
+    Console.WriteLine($"Transactioned signed by user '{signature.PublicKeyBase58}'.");
+}, credentialsRequiredOption, transactionPathOption);
+#endregion
+
+var rootCommand = new RootCommand
+{
+    createKeysCommand,
+    createBlockchainCommand,
+    validateBlockchainCommand,
+    signTransactionCOmmand
+};
+rootCommand.Description = "FangChain CLI";
+
+return await rootCommand.InvokeAsync(args);
